d4111c580b3ca323fdd66e495020a54685f7104d
Gyaneshwar Giri <sangamgiri5813@gmail.com>
2026-08-06 23:50:07 +0530

    Learning python day-3

---
+++ day3.py
@@ -0,0 +1,4 @@
+#using list to store multiple values and print them
+fruits=["apple","banana","orange","mango"]
+print(fruits[0])#to print first value of list
+print(fruits[3])#to print last value of list
