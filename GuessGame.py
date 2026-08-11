6089d3f36c67994fd0ec6fa4b9970bf96b82fb30
Gyaneshwar Giri <sangamgiri5813@gmail.com>
2026-08-11 20:54:05 +0530

    First mini project

--- GuessGame.py
+++ GuessGame.py
@@ -3,7 +3,7 @@ import random
 def game():
     guess=random.randint(1,100)
     while True:
-        a=input("Guess a number between 1 and 100:")
+        a=input("Guess a number between 1 and 100: ")
         if guess==int(a):
             print("You guessed it! game over.")
             break
